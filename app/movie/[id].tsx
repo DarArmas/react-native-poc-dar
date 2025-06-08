e32157595b13fb6a40a027ed9863b84ec26b9059
Dar <darnellsanchez2011@gmail.com>
2025-06-08 13:43:10 -0600

    Get latest movies from TMDB

--- app/movie/[id].tsx
+++ app/movie/[id].tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import {useLocalSearchParams} from "expo-router";
 
 const MovieDetails = () => {
+    const {id} = useLocalSearchParams();
+
     return (
         <View>
-            <Text>Movie details</Text>
+            <Text>Movie details: {id}</Text>
         </View>
     );
 };
