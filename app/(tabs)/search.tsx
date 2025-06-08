5c22e2ef047cf74639af163752ba7276c2c53d03
Dar <darnellsanchez2011@gmail.com>
2025-06-08 11:56:17 -0600

    Add routing

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Saved = () => {
+    return (
+        <View>
+            <Text>Search</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default Saved;
---
+++ app/movies/[id].tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const MovieDetails = () => {
+    return (
+        <View>
+            <Text>Movie details</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default MovieDetails;
